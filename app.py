cea4b5d5880cb1a0a1551f4c307504a1ed34b86c
Arnav Doss <arnavdoss@gmail.com>
2022-02-09 20:41:57 +0100

    changes to get Heroku to work

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ import os
 import dash_bootstrap_components as dbc
 
 # Initialize dash app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY], title='MATool', update_title=None,
-                suppress_callback_exceptions=True,
-                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}])
-# LUMEN, SANDSTONE, YETI, CERULEAN, MATERIA,
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 server = app.server
 server.secret_key = os.environ.get('secret_key', 'secret')
--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 from app import app, server
+import dash_bootstrap_components as dbc
 from BettingRPS.player_view import layout as layout_betting_rps
 
-layout = layout_betting_rps
+app.layout = dbc.Container([layout_betting_rps], fluid=True)
 
 if __name__ == '__main__':
-    app.layout = layout
     app.run_server(debug=False)
